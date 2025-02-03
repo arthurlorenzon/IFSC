537516364a439f948eadbd409e44ac7e57f4ffd1
arthurlorenzon <arthurgabriellp@gmail.com>
2025-02-03 19:58:20 -0300

    adicionar testes Pessoa

---
+++ aula_teste/app/src/test/java/org/example/PessoaTest.java
@@ -0,0 +1,39 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PessoaTest {
+
+    @Test
+    void constructor() {
+        assertThrows(IllegalArgumentException.class, () -> {
+           Pessoa p = new Pessoa("Maria", "");
+        });
+        assertDoesNotThrow(() -> {
+            Pessoa p = new Pessoa("João", "12345678909");
+        });
+    }
+
+    @Test
+    void setNome() {
+        Pessoa p = new Pessoa("João", "12345678909");
+        assertEquals("João", p.getNome());
+        assertThrows(IllegalArgumentException.class, () -> {
+            p.setNome(null);
+        });
+        assertThrows(IllegalArgumentException.class, () -> {
+            p.setNome("");
+        });
+    }
+
+    @Test
+    void testCpf() {
+        Pessoa p = new Pessoa("Maria", "07021303971");
+        assertTrue(p.isValidCPF(p.getCpf()));
+        Pessoa j = new Pessoa("Maria", "00000000000");
+        assertFalse(j.isValidCPF(j.getCpf()));
+    }
+
+}
